e3324ad46f5a7e70e49e4d543c18184d0153d154
shaikh arsalan <shaikharsalan23197@gmail.com>
2017-08-15 23:36:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('change_text');
 element.innerHTML = 'this text is change by javascript';
 
 //Move element on click 
-var img = document.getElementById('dina');
+var img = document.getElementById('madi');
 var marginleft = 0;
 function moveRight(){
     marginleft = marginleft + 10;
