0953a60314aaeabefc470523439e3261f81979de
shaikh arsalan <shaikharsalan23197@gmail.com>
2017-08-15 23:34:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
