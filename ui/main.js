52e0623cbe7d88ca3bf6af6cc3474454b349ef1d
shaikh arsalan <shaikharsalan23197@gmail.com>
2017-08-15 23:38:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ element.innerHTML = 'this text is change by javascript';
 
 //Move element on click 
 var img = document.getElementById('madi');
-var marginleft = 0;
+var marginLeft = 0;
 function moveRight(){
-    marginleft = marginleft + 10;
-    img.style.marginleft = marginleft + 'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveRight,100);
